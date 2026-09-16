134eef346ec8b5e672ddc11e1565ab8b75b3dc80
Sameer Al Sahab <jahirmondol5456@gmail.com>
2026-09-17 00:28:25 +0600

    root: blue_fish.sh main

---
+++ blue_fish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Copyright (c) 2026 Sameer Al Sahab
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+#TODO for next time (I am lazy af!!)
+# Add a good ANSII logo for project 
+# --mode, --target-link flags for blue_fish.sh
+# Deps check using my existing check_dependencies 
+
